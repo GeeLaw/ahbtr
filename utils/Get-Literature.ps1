83d08115bd43b99c2a69f075dba8abba56f4858e
Gee Law <geelaw@outlook.com>
2022-03-24 00:13:37 -0700

    Literature retrieval script.

---
+++ utils/Get-Literature.ps1
@@ -0,0 +1,20 @@
+$CryptoBib = [System.IO.Path]::Combine($PSScriptRoot, '..', 'paper', 'crypto.bib');
+$LiteratureCsv = [System.IO.Path]::Combine($PSScriptRoot, 'Literature.csv');
+$content = Get-Content -LiteralPath $CryptoBib;
+$matches = Select-String -LiteralPath $CryptoBib -Pattern 'ad[ -]?hoc|de-?central|broad-?cast|revoke|revoking|revocation|trace(able)?|tracing';
+$matches = $matches | Where-Object Line -Like '*title*';
+$pattern_id = [regex]'^@.*?\{(.*?),$';
+$pattern_title = [regex]'^\s*title\s*=\s*"(.*?)",';
+$entries = $matches | ForEach-Object {
+  $i = $_.LineNumber - 1;
+  While ($content[$i] -NotLike '@*{*,')
+  {
+    $i = $i - 1;
+  }
+  [pscustomobject]@{
+    'Id' = $pattern_id.Replace($content[$i], '$1');
+    'Title' = $pattern_title.Replace($_.Line, '$1')
+  };
+};
+$entries = $entries | Sort-Object Title, Id;
+$entries | ConvertTo-Csv -NoTypeInformation | Set-Content $LiteratureCsv;
