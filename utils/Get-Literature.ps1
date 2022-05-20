e1e35c2082a56d82e92ff4bc2f2f2d43eec66bb4
Gee Law <geelaw@outlook.com>
2022-05-20 03:16:09 -0400

    Update literature filter.

--- utils/Get-Literature.ps1
+++ utils/Get-Literature.ps1
@@ -1,7 +1,7 @@
 $CryptoBib = [System.IO.Path]::Combine($PSScriptRoot, '..', 'paper', 'crypto.bib');
 $LiteratureCsv = [System.IO.Path]::Combine($PSScriptRoot, 'Literature.csv');
 $content = Get-Content -LiteralPath $CryptoBib;
-$matches = Select-String -LiteralPath $CryptoBib -Pattern 'ad[ -]?hoc|de-?central|broad-?cast|revoke|revoking|revocation|trace(able)?|tracing|piracy|pirate|water-?mark|(multi[- ]input).*?(fe|functional encryption)';
+$matches = Select-String -LiteralPath $CryptoBib -Pattern 'ad[ -]?hoc|de-?central|broad-?cast|revoke|revoking|revocation|trace(able)?|tracing|piracy|pirate|water-?mark|(multi[- ](input|authority)).*?(fe|abe|encryption)';
 $matches = $matches | Where-Object Line -Like '*title*';
 $pattern_id = [regex]'^@.*?\{(.*?),$';
 $pattern_title = [regex]'^\s*title\s*=\s*"(.*?)",';
